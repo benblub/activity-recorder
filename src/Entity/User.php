01e3a044dc8d01df60549741d5e2ed00eb62542f
Benjamin Knecht <knecht.benjamin@yahoo.de>
2020-11-29 20:42:04 +0100

    add User ApiResource config, validation, serialation
    We config which fields are exposed on the Api. Some Basic validation for fields and we want plainPassword use as named password

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,38 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("email", message="Email Adress is allready in use")
+ * @ApiResource(
+ *     normalizationContext={"groups"={"user:read"}},
+ *     denormalizationContext={"groups"={"user:write"}},
+ *     collectionOperations={
+ *          "post" = {
+ *              "validation_groups" = { "Default", "create" }
+ *          },
+ *     },
+ *     itemOperations={
+ *          "get" = {
+ *              "security" = "is_granted('ROLE_USER') and object == user",
+ *              "validation_groups" = { "Default" }
+ *          },
+ *          "put" = {
+ *              "security" = "is_granted('ROLE_USER') and object == user",
+ *              "validation_groups" = { "Default" }
+ *          },
+ *     },
+ * )
  */
 class User implements UserInterface
 {
@@ -17,11 +41,15 @@ class User implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"user:read"})
      */
-    private $id;
+    protected $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"user:read", "user:write"})
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
@@ -37,9 +65,31 @@ class User implements UserInterface
     private $password;
 
     /**
+     * @Groups({"user:write"})
+     * @SerializedName("password")
+     */
+    private $plainPassword;
+
+    /**
      * @ORM\OneToMany(targetEntity=Activity::class, mappedBy="user", orphanRemoval=true)
+     * @Groups({"user:read"})
      */
     private $activities;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $enabled;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $activateToken;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apiToken;
 
     public function __construct()
     {
@@ -150,6 +200,54 @@ class User implements UserInterface
                 $activity->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(?bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    public function getActivateToken(): ?string
+    {
+        return $this->activateToken;
+    }
+
+    public function setActivateToken(?string $activateToken): self
+    {
+        $this->activateToken = $activateToken;
+
+        return $this;
+    }
+
+    public function getApiToken(): ?string
+    {
+        return $this->apiToken;
+    }
+
+    public function setApiToken(string $apiToken): self
+    {
+        $this->apiToken = $apiToken;
 
         return $this;
     }
