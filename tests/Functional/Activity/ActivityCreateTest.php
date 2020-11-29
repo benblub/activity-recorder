09b8e6109478dc6de4345dde46ca3911b7f7889b
Benjamin Knecht <knecht.benjamin@yahoo.de>
2020-11-29 17:41:03 +0100

    test create activits pass related user and send apiToken

--- tests/Functional/Activity/ActivityCreateTest.php
+++ tests/Functional/Activity/ActivityCreateTest.php
@@ -14,12 +14,18 @@ class ActivityCreateTest extends CustomApiTestCase
     {
         $client = self::createClient();
 
+        $user = $this->createUser();
+
         $d = new DateTime();
         $client->request('POST', '/api/activities', [
             'json' => [
                 'activityDate' => $d->format('Y-m-d'),
                 'performendTime' => 2.5,
-                'description' => 'awesome code created!'
+                'description' => 'awesome code created!',
+                'user' => '/api/users/8'
+            ],
+            'headers' => [
+                'X-AUTH-TOKEN' => $user->getApiToken()
             ]
         ]);
         $this->assertResponseStatusCodeSame(self::RESOURSE_CREATED_201);
