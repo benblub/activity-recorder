1f34572a3a3155e5c017a2f55d01e23455bb0a68
Benjamin Knecht <knecht.benjamin@yahoo.de>
2020-11-30 23:15:54 +0100

    cleanup

--- tests/Functional/User/UserLoginTest.php
+++ tests/Functional/User/UserLoginTest.php
@@ -5,6 +5,10 @@ namespace App\Tests\Functional\User;
 
 
 use App\Test\CustomApiTestCase;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class UserLoginTest extends CustomApiTestCase
 {
@@ -12,10 +16,10 @@ class UserLoginTest extends CustomApiTestCase
      * We test here to retrive an apiToken from Post Request with email and password.
      * The User dont get authenticated here, for LoginFormAuth call /weblogin instead
      *
-     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
      */
     public function testLogin()
     {
