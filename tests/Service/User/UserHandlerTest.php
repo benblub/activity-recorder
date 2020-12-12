168e588b388454ac5fa5e90e613e52a29712d6ee
Benjamin Knecht <knecht.benjamin@yahoo.de>
2020-12-12 12:25:40 +0100

    user userFactory

--- tests/Service/User/UserHandlerTest.php
+++ tests/Service/User/UserHandlerTest.php
@@ -2,14 +2,28 @@
 
 namespace App\tests\Service\User;
 
-use App\Entity\User;
+
+use App\Factory\UserFactory;
 use PHPUnit\Framework\TestCase;
 
 class UserHandlerTest extends TestCase
 {
+    /**
+     * @var UserFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new UserFactory();
+    }
+
     public function testActivate()
     {
-        $user = new User();
+        $user = $this->factory->createUser('mail@mail.com', false, ['ROLE_USER']);
+
+        $this->assertEquals(false, $user->getEnabled());
+
         $user->setEnabled(true);
 
         $this->assertEquals(true, $user->getEnabled());
