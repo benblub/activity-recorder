1719f427b5d7110af2ee35b0debc5e87590a189e
Benjamin Knecht <knecht.benjamin@yahoo.de>
2021-02-06 15:07:53 +0100

    fix TypeError user must be instance of Class User and not null

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -24,6 +24,7 @@ class UserTest extends TestCase
         $activity = new Activity();
 
         $user = new User();
+        $activity->setUser($user);
         $user->addActivity($activity);
         $user->removeActivity($activity);
 
