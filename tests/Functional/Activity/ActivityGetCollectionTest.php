28c7df18acf1994d2eb377d64a3d6c27a21cf112
Benjamin Knecht <knecht.benjamin@yahoo.de>
2021-02-07 11:32:07 +0100

    add test autofilter activities for role user

--- tests/Functional/Activity/ActivityGetCollectionTest.php
+++ tests/Functional/Activity/ActivityGetCollectionTest.php
@@ -15,7 +15,7 @@ class ActivityGetCollectionTest extends CustomApiTestCase
         $activity = $this->createActivity();
         $this->createActivity();
 
-        $response = $client->request('GET', '/api/activities', [
+        $client->request('GET', '/api/activities', [
             'headers' => [
                 'X-AUTH-TOKEN' => $activity->getUser()->getApiToken()
             ]
@@ -23,5 +23,45 @@ class ActivityGetCollectionTest extends CustomApiTestCase
 
         $this->assertResponseHeaderSame('Content-Type', 'application/ld+json; charset=utf-8');
         $this->assertResponseStatusCodeSame(self::RESOURSE_RESPONSE_200);
+    }
+
+    /**
+     * Test For ROLE_USER collection is auto filtered
+     */
+    public function testGetCollectionActivityIsAutoFiltered()
+    {
+        $client = self::createClient();
+
+        $activity = $this->createActivity();
+        $this->createActivity();
+
+        $client->request('GET', '/api/activities', [
+            'headers' => [
+                'X-AUTH-TOKEN' => $activity->getUser()->getApiToken()
+            ]
+        ]);
+
+        $this->assertJsonContains(['hydra:totalItems' => 1]);
+    }
+
+    /**
+     * Test For ROLE_ADMIN collection is not auto filtered
+     */
+    public function testGetCollectionActivityIsNotAutoFiltered()
+    {
+        $client = self::createClient();
+
+        $admin = $this->createUser('ROLE_ADMIN');
+        $activity = $this->createActivity();
+        $this->createActivity();
+
+        $response = $client->request('GET', '/api/activities', [
+            'headers' => [
+                'X-AUTH-TOKEN' => $admin->getApiToken()
+            ]
+        ]);
+
+        $data = json_decode($response->getContent(), true);
+        $this->assertGreaterThan(1, $data["hydra:totalItems"]);
     }
 }
